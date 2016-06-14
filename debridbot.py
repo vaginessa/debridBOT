c0d39573ca0628626bf1f89f6c322516ee6b642c
gabb96 <gabriele.dauriz@gmail.com>
2016-06-14 16:18:15 +0200

    Create debridbot.py

---
+++ debridbot.py
@@ -0,0 +1,132 @@
+#-*- coding:utf-8 -*-
+#installare le dipendenze
+#python -m pip install --upgrade pip
+#pip install pyTelegramBotAPI
+#pip install psutil
+#pip install requests
+import telebot
+import psutil
+import sys
+import requests
+from pprint import pprint
+from telebot import types
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+#admin = [line.rstrip('\n') for line in open('admin.txt','rt')]
+API_TOKEN = '115094957:AAGvwxDTrJE0wCDAORNwFzTs88dNc2p-muc'
+URL = 'http://www.alldebrid.com/register/?action=login&login_login=xxxxxx&login_password=xxxxxx'
+PROCNAME = "a.exe"
+bot = telebot.TeleBot(API_TOKEN)
+
+admin = [line.rstrip('\n') for line in open('admin.txt','rt')]
+
+
+def addAdmin(name,id):
+    global admin
+    with open('admin.txt', 'a+') as f:
+        f.write("\n"+str(name)+"\n"+str(id))
+    #refresh admin list
+    admin = [line.rstrip('\n') for line in open('admin.txt','rt')]
+        
+def isAdmin(var):
+    #bot.send_message(var, "%s"%(var))
+    #bot.send_message(var, "%s"%(admin))
+    if str(var) in admin:
+        
+        return True
+    else:
+        return False
+def debridit(link):
+    session = requests.session()
+        # Authenticate
+    r = session.get(URL)
+    initial_link = 'http://www.alldebrid.com/service.php?json=true&link=%s'%(link)
+        # Try accessing a page that requires you to be logged in
+        #bot.send_message(cid, 'http://www.alldebrid.com/service.php?json=true&link=%s'%(message.text))
+    r = session.get(initial_link)
+    var= str(r.content)
+    words = var.split(",")
+        #print words[0]
+    link = words[0].split("\"")
+    final_link = link[3].replace("\\","")
+    if str(final_link[-1:]) == '?':
+        bot.send_message(cid, 'link non valido')
+    else:
+        bot.send_message(cid, final_link)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    cid = message.chat.id
+    if isAdmin(cid):
+        bot.send_message(cid,"ok")
+    else:
+        bot.send_message(cid, "NON HAI I PERMESSI")
+    
+
+@bot.message_handler(commands=['id'])
+def id(m):
+    cid = m.chat.id
+    bot.send_message( cid, "Ciao %s , il tuo  ID e' %s.\n" %(m.from_user.first_name, cid))
+
+@bot.message_handler(commands=['admin'])
+def id(m):
+    cid = m.chat.id
+    name = m.from_user.first_name
+    if isAdmin(cid) :
+        #bot.send_message( cid, "Ciao %s , sei un admin\n" %(m.from_user.first_name))
+        bot.send_message( cid, "%s" %(admin))
+    else:
+        bot.send_message( cid, "Ciao %s , NON sei un admin\n" %(m.from_user.first_name))
+    pass
+
+@bot.message_handler(commands=['link'])
+def link(m):
+    cid = m.chat.id
+    if len(m.text.split()) != 2:
+        bot.send_message(cid, " /addAdmin <LINK>")
+        return
+    try:
+        link = str(m.text.split()[1])
+    except:
+        bot.send_message(cid, " /addAdmin <LINK>")
+        return
+            # Start a session so we can have persistant cookies
+    session = requests.session()
+        # Authenticate
+    r = session.get(URL)
+    initial_link = 'http://www.alldebrid.com/service.php?json=true&link=%s'%(link)
+        # Try accessing a page that requires you to be logged in
+        #bot.send_message(cid, 'http://www.alldebrid.com/service.php?json=true&link=%s'%(message.text))
+    r = session.get(initial_link)
+    var= str(r.content)
+    words = var.split(",")
+        #print words[0]
+    link = words[0].split("\"")
+    final_link = link[3].replace("\\","")
+    if str(final_link[-1:]) == '?':
+        bot.send_message(cid, 'link non valido')
+    else:
+        bot.send_message(cid, final_link)
+    pass
+
+
+@bot.message_handler(commands=['addAdmin'])
+def setNewAdmin(m):
+    cid = m.chat.id
+    if len(m.text.split()) != 3:
+        bot.send_message(cid, " /addAdmin <NOME> <ID>")
+        return
+    try:
+        idNADMIN = int(m.text.split()[2])
+        name = str(m.text.split()[1])
+    except:
+        bot.send_message(cid, " /addAdmin <NOME> <ID>")
+        return
+    bot.send_message(cid, "{} è stato aggiunto agli amministratori con il codice id {}".format(name,idNADMIN))
+    addAdmin(name,idNADMIN)
+    pass
+    
+		
+
+bot.polling()
